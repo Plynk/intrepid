07ea297bc99027063f1e5f3a3d8116a9b5d1c35c
Luke Thayer <lukemanthayer@gmail.com>
2023-01-02 16:08:30 -0800

    broadcast and audience trait

--- src/intrepid.rs
+++ src/intrepid.rs
@@ -1,33 +1,109 @@
 use anyhow::{Ok, Result};
-use std::net::UdpSocket;
+use std::{net::UdpSocket, sync::Arc};
 
 const BROADCAST_SENDER: &str = "255.255.255.255";
 const BROADCAST_LISTNER: &str = "0.0.0.0";
+const BROADCAST_PORT : &str = "6401";
+const BROADCAST_BUFFER_SIZE: usize = 10;
 
-struct UDPNode {
+pub struct UDPNode {
     bind_ip: String,
     send_ip: String,
     port: String,
     tx: std::sync::mpsc::Sender<Vec<u8>>,
     rx: std::sync::mpsc::Receiver<Vec<u8>>,
+    broad_cast_socket: Arc<UdpSocket>,
 }
 
 impl UDPNode {
     pub fn new(port: String, bind_ip: String, send_ip: String) -> UDPNode {
         let (tx, rx) = std::sync::mpsc::channel();
+
         UDPNode {
             bind_ip,
             send_ip,
-            port,
+            port: port.clone(),
             tx,
             rx,
+            broad_cast_socket: Arc::new(
+                UdpSocket::bind(format!("{BROADCAST_LISTNER}:{BROADCAST_PORT}"))
+                    .expect("failed to bind broadcast socket"),
+            ),
         }
     }
 }
 
-trait IntrepidSocket {
-    fn listening_thread() -> std::sync::mpsc::Receiver<Vec<u8>>;
-    fn sending_thread() -> std::sync::mpsc::Sender<Vec<u8>>;
-    fn broadcast_thread() -> std::sync::mpsc::Sender<Vec<u8>>;
-    fn audience_thread() -> std::sync::mpsc::Receiver<Vec<u8>>;
+impl IntrepidSocket for UDPNode {
+    fn listening_thread(&self) -> std::sync::mpsc::Receiver<Vec<u8>> {
+        let (tx, rx) = std::sync::mpsc::channel();
+        rx
+    }
+    fn sending_thread(&self) -> std::sync::mpsc::Sender<Vec<u8>> {
+        let (tx, rx) = std::sync::mpsc::channel();
+        tx
+    }
+    fn broadcast_thread(
+        &self,
+    ) -> Result<(
+        std::sync::mpsc::Sender<Vec<u8>>,
+        Box<dyn Fn() -> Result<()> + Send>,
+    )> {
+        let socket = self.broad_cast_socket.clone();
+        let (tx, rx) = std::sync::mpsc::channel();
+        socket.set_read_timeout(Some(std::time::Duration::new(5, 0)))?;
+        socket.set_broadcast(true)?;
+        println!("Broadcast: {:?}", socket.broadcast());
+        println!("Timeout: {:?}", socket.read_timeout());
+
+        Ok((
+            tx,
+            Box::new(move || {
+                loop {
+                    let msg = rx.recv().expect("BroadCast sender hung up");
+                    socket.send_to(&msg[..],format!("{BROADCAST_SENDER}:{BROADCAST_PORT}"));
+                    println!("Sent Broadcast");
+                    std::thread::sleep(std::time::Duration::from_millis(1000));
+                }
+                Ok(())
+            }),
+        ))
+    }
+    fn audience_thread(
+        &self,
+    ) -> Result<(
+        std::sync::mpsc::Receiver<Vec<u8>>,
+        Box<dyn Fn() -> Result<()> + Send>,
+    )> {
+        let (tx, rx) = std::sync::mpsc::channel();
+        let socket = self.broad_cast_socket.clone();
+        Ok((
+            rx,
+            Box::new(move || {
+                let mut buf = [0; BROADCAST_BUFFER_SIZE];
+                loop {
+                    let (amt, src) = socket.recv_from(&mut buf)?;
+                    println!("Recieved Broadcast from : {src:?}");
+                    tx.send(buf.to_vec()).expect("Audience Receiver hung up");
+                }
+                Ok(())
+            }),
+        ))
+    }
+}
+
+pub trait IntrepidSocket {
+    fn listening_thread(&self) -> std::sync::mpsc::Receiver<Vec<u8>>;
+    fn sending_thread(&self) -> std::sync::mpsc::Sender<Vec<u8>>;
+    fn broadcast_thread(
+        &self,
+    ) -> Result<(
+        std::sync::mpsc::Sender<Vec<u8>>,
+        Box<dyn Fn() -> Result<()> + Send>,
+    )>;
+    fn audience_thread(
+        &self,
+    ) -> Result<(
+        std::sync::mpsc::Receiver<Vec<u8>>,
+        Box<dyn Fn() -> Result<()> + Send>,
+    )>;
 }
